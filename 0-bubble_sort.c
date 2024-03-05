07d88a9405e78b9264a59047fb15c2a773a09f4c
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-03-05 23:55:11 +0000

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+#include <string.h>
+
+/**
+ * bubble_sort - sort function
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: 0 on success
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, swapped;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
+}
